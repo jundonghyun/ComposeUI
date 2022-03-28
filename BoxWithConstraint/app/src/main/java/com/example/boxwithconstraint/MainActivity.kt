d607bae17d193bdcca9ca06caa8ffc8b9d9123c8
jundonghyun <jundonghyun>
2022-03-28 10:17:51 +0900

    BoxConstraint 수정

--- BoxWithConstraint/app/src/main/java/com/example/boxwithconstraint/MainActivity.kt
+++ BoxWithConstraint/app/src/main/java/com/example/boxwithconstraint/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.boxwithconstraint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.boxwithconstraint.ui.theme.BoxWithConstraintTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    BoxWithConstraintContainer()
                 }
             }
         }
@@ -30,14 +34,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun BoxWithConstraintContainer(){
+    BoxWithConstraints(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.White),
+        Alignment.Center,
+        propagateMinConstraints = false
+    ) {
+
+        Column() {
+            BoxWithConstraintsItems(modifier = Modifier
+                .size(200.dp)
+                .background(Color.Yellow)
+            )
+            BoxWithConstraintsItems(modifier = Modifier
+                .size(300.dp)
+                .background(Color.Green)
+            )
+        }
+    }
 }
+
+@Composable
+fun BoxWithConstraintsItems(modifier: Modifier){
+    BoxWithConstraints(
+        modifier = modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center,
+        propagateMinConstraints = false
+    ) {
+        if(this.minWidth > 200.dp){
+            Text(text = "이것은 큰 상자이다")
+        }
+        else Text(text = "이것은 작은 상자이다")
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BoxWithConstraintTheme {
-        Greeting("Android")
+        BoxWithConstraintContainer()
     }
 }
